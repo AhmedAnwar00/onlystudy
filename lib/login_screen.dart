21fe39a6df353690e903675639063138653364bc
MSI\ahmed <ahmed.abdalla.kr@gmail.com>
2026-01-12 17:23:38 +0200

    --

---
+++ lib/login_screen.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/material.dart';
+
+class login_scrren extends StatelessWidget {
+  const login_scrren ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("asd",
+      style: TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+        color: Colors.black
+
+      ),
+      )),
+    );
+  }
+}
